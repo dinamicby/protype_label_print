a14d37cc4800637ff70e6c5e525e3af9996704fd
dinamicby <kis02@bk.ru>
2025-08-14 18:44:51 +0200

    fix

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version="0.0.2",
     description="Bulk label printing for Items on A4 with QR",
     author="Protype",
-    app_description = "Bulk label printing for Items on A4 with QR"         
+    app_description = "Bulk label printing for Items on A4 with QR",      
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
